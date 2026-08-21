1b449cca4138f3c45cfc79784201d1e6c24622b1
Riddhi Chavda <riddhichavda59@gmail.com>
2026-08-21 15:20:54 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,61 @@
+# Python File Handling
+
+# text file handling
+
+# open() function
+
+#  file = open('filename.txt' , 'mode' )
+
+'''
+Mode       Meaning
+
+r              Read
+w             Write
+a              Append
+x              create new file
+r+             Read  + Write
+w+            Write + Read
+a+              Append + Read
+r-              Read
+'''
+
+files = open("demo.txt" , "w")
+
+files.write("vivek\n")
+files.write("Python\n")
+files.write("Red and White\n")
+
+files = open("demo.txt" , "r")
+
+data = files.read()
+
+print(data)
+
+files.close()
+
+file = open("demo.txt",  "a")
+
+file.write("Javascript")
+
+file = open("demo.txt" , "r")
+
+data = file.read()
+
+print(data)
+
+file.close()
+
+file = open("demo.txt" , "r")
+
+print(file.readline())
+print(file.readline())
+
+print(file.readlines())
+
+
+
+
+
+
+
 
